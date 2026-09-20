6152c603cc92a225cfb5e04b6d35762168c829b2
Laiba Shahzadi <heer097531@gmail.com>
2026-09-20 00:57:32 -0700

    First Commit - Hangman Game for CodeAlpha Internship

---
+++ Hangman Game.py
@@ -0,0 +1,46 @@
+#-------Python Intenship Project------
+
+import random
+list_of_words = ["laiba", "intern", "Pakistan", "Python", "Cyber_security"]
+word = random.choice(list_of_words)
+
+guessed_letters = []
+incorrect_guesses = 0
+max_guesses = 6
+print("\n---Welcome to Hangman Game---\n===================================")
+print(f"You have to {max_guesses} incorrect guesses allowed.\n")
+
+while incorrect_guesses < max_guesses:
+    display = " "
+    for char in word:
+        if char in guessed_letters:
+            display +=char +" "
+        else:
+            display += "-"
+    print("\nWord: ",display)
+
+    if "-" not in display:
+        print("Congratulations🎉, you Won..!!")
+        break
+
+    guess = input("Enter a single letter: ").lower()
+    if guess in guessed_letters:
+        print("You already have to guessed, try another.")
+        continue
+    guessed_letters.append(guess)
+    if guess in word:
+        print("Correct Answer! This letter is involved into word.")
+    else:
+        incorrect_guesses+=1
+        rermaining = max_guesses-incorrect_guesses
+        print(f"THis attempt is wrong! you have {rermaining} remaining attempts.")  
+    if incorrect_guesses == max_guesses:
+        print("\n Game over! You lost. ")
+        print("The correct word is: ", word)              
+
+        
+
+
+
+
+
